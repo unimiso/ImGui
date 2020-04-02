746ee44aaeddd9525265d0eeee3c3a5a7923bf8d
omar <omarcornut@gmail.com>
2020-04-02 16:46:53 +0200

    RangeSelect/MultiSelect: Fix testing key mods from after the nav request (remove need to hold the mod longer)

--- imgui_internal.h
+++ imgui_internal.h
@@ -2111,6 +2111,7 @@ struct ImGuiContext
     bool                    MultiSelectEnabled;
     ImGuiMultiSelectFlags   MultiSelectFlags;
     ImGuiMultiSelectState   MultiSelectState;                   // We currently don't support recursing/stacking multi-select
+    ImGuiKeyChord           MultiSelectKeyMods;
 
     // Render
     float                   DimBgRatio;                         // 0.0..1.0 animation when fading in a dimming background (for modal window and CTRL+TAB list)
@@ -2366,6 +2367,7 @@ struct ImGuiContext
 
         MultiSelectEnabled = false;
         MultiSelectFlags = ImGuiMultiSelectFlags_None;
+        MultiSelectKeyMods = ImGuiMod_None;
 
         DimBgRatio = 0.0f;
 
